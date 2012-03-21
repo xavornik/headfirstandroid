fa42931839a007dcd16bab878ea8fbbdd677e4e7
geroen@headfirstandroid.com <geroen@headfirstandroid.com@cbf1a9a6-ae1e-aba1-d064-328df3d57495>
2012-03-21 22:09:01 +0000

    Share project "HFA_Chapter_10" into "https://headfirstandroid.googlecode.com/svn"

---
+++ trunk/HFA_Chapter_10/src/com/headfirstandroid/hfa_chapter_10/CoolActivity.java
@@ -0,0 +1,13 @@
+package com.headfirstandroid.hfa_chapter_10;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoolActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coollayout);
+    }
+}
